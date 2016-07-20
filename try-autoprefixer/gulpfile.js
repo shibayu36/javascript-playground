016537479b84704f0b5be3f4f593f57de0a65d1f
shibayu36 <shibayu36@gmail.com>
2016-07-20 10:18:31 +0900

    less

--- try-autoprefixer/gulpfile.js
+++ try-autoprefixer/gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var less = require('gulp-less');
 
 gulp.task('compile-css', function () {
     return gulp.src(['src/css/*.css'])
+        .pipe(autoprefixer({
+            browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('static/css'));
+});
+
+gulp.task('compile-css-with-less', function () {
+    return gulp.src(['src/less/*.less'])
+        .pipe(less())
         .pipe(autoprefixer({
             browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
             cascade: false
